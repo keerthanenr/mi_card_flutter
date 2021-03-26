abd6b8dee4d8a914948cef8248f1918d1495a857
keerthanenr <keerthanenr@live.co.uk>
2021-03-26 14:35:01 +0000

    Created with Row and Card element

--- lib/main.dart
+++ lib/main.dart
@@ -32,46 +32,50 @@ class MyApp extends StatelessWidget {
                     fontSize: 20.0,
                     color: Colors.white,
                   )),
-              Container(
-                  padding: EdgeInsets.all(10.0),
+              Card(
                   color: Colors.white,
                   margin:
                       EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                  child: Row(children: <Widget>[
-                    Icon(
-                      Icons.phone,
-                      color: Colors.teal,
-                    ),
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    Text('+44 123 456 789',
-                        style: TextStyle(
-                          color: Colors.teal.shade900,
-                          fontFamily: 'SourceSansPro',
-                          fontSize: 20.0,
-                        ))
-                  ])),
-              Container(
-                  padding: EdgeInsets.all(10.0),
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Row(children: <Widget>[
+                      Icon(
+                        Icons.phone,
+                        color: Colors.teal,
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text('+44 123 456 789',
+                          style: TextStyle(
+                            color: Colors.teal.shade900,
+                            fontFamily: 'SourceSansPro',
+                            fontSize: 20.0,
+                          ))
+                    ]),
+                  )),
+              Card(
                   color: Colors.white,
                   margin:
                       EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                  child: Row(children: <Widget>[
-                    Icon(
-                      Icons.email,
-                      color: Colors.teal,
-                    ),
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    Text('keerthanenr@gmail.com',
-                        style: TextStyle(
-                          color: Colors.teal.shade900,
-                          fontFamily: 'SourceSansPro',
-                          fontSize: 20.0,
-                        ))
-                  ]))
+                  child: Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Row(children: <Widget>[
+                      Icon(
+                        Icons.email,
+                        color: Colors.teal,
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text('email@gmail.com',
+                          style: TextStyle(
+                            color: Colors.teal.shade900,
+                            fontFamily: 'SourceSansPro',
+                            fontSize: 20.0,
+                          ))
+                    ]),
+                  ))
             ],
           ),
         ),
